91226ecdd2f4aab6d30e39a9508797bbc40df3c3
Ankit Jain <ankii.jain@gmail.com>
2022-11-11 14:02:39 +0530

    bucket optimization

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,25 @@
+output "bucket_arn" {
+    description = "DevOps Bucket ARN"
+    value = local.create_devops_bucket ? module.devops_bucket[0].arn : ""
+}
+
+output "codebuild_service_role" {
+    description = "ARN of IAM Role for CodeBuild Service"
+    value = var.create_codebuild_service_role ? module.iam_devops[0].service_linked_roles[local.codebuild_role_name].arn : null
+}
+
+output "codepipeline_service_role" {
+    description = "ARN of IAM Role for CodePipeline Service"
+    value = var.create_codepipeline_service_role ? module.iam_devops[0].service_linked_roles[local.codepipeline_role_name].arn : null
+}
+
 output "codecommit_repository" {
     description = "CodeCommit Repository Attributes"
     value = var.create_repository ? module.code_commit[0].repository : {}
 }
 
 output "codebuild_projects" {
-    description = "CodeBuild Stages Attributes"
+    description = "CodeBuild Project Attributes"
     value = local.create_build_projects ? module.code_build[0].projects : {}
 }
 
@@ -13,14 +28,10 @@ output "codepipeline" {
     value = local.create_pipeline ? module.code_pipeline[0].pipeline: {}
 }
 
-output "codebuild_bucket_arn" {
-    description = "Code Build Bucket ARN"
-    value = local.create_codebuild_bucket ? module.codebuild_bucket[0].arn : ""
-}
-
-output "codepipeline_bucket_arn" {
-    description = "Code Pipeline Bucket ARN"
-    value = local.create_codepipeline_bucket ? module.codepipeline_bucket[0].arn : ""
+output "ssm_parameter_webhook_secret" {
+    description = "SSM parameter where webhook secret is stored."
+    value = (var.enable_webhook 
+                    && var.generate_webhook_secret) ? module.webhook[0].ssm_parameter_webhook_secret : ""
 }
 
 output "kms_key" {
@@ -30,10 +41,4 @@ output "kms_key" {
                                     "arn"    = module.encryption[0].key_arn 
                                     "policy" = module.encryption[0].key_policy 
                                 } : null
-}
-
-output "ssm_parameter_webhook_secret" {
-    description = "SSM parameter were webhook secret is stored."
-    value = (var.enable_webhook 
-                    && var.generate_webhook_secret) ? module.webhook[0].ssm_parameter_webhook_secret : ""
 }
